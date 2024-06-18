bcb903051c9c4f1f469b01a18d258396e48267c7
hackout <hackout@vip.qq.com>
2024-06-18 14:27:42 +0800

    Select

--- src/Services/Work/MakeSelect.php
+++ src/Services/Work/MakeSelect.php
@@ -13,25 +13,21 @@ class MakeSelect
 {
     public function run(mixed $model, mixed $builder, $select = []): mixed
     {
-        if ($select) {
-            $builder = $builder->select($select);
-        } else {
-            $selectList = collect(array_keys($model->getCasts()))->concat(collect($model->getFillable()))->unique();
-            $casts = $model->getCasts();
-            $selectArr = [];
-            $selectList->each(function ($field) use ($casts, &$selectArr) {
-                if (array_key_exists($field, $casts)) {
-                    if (class_exists($casts[$field]) && strpos($casts[$field], 'Casts') !== false) {
-                        $selectArr[] = (new $casts[$field])->select($field);
-                    } else {
-                        $selectArr[] = $field;
-                    }
+        $selectList = collect($select ? $select : $model->getFillable());
+        $casts = $model->getCasts();
+        $selectArr = [];
+        $selectList->each(function ($field) use ($casts, &$selectArr) {
+            if (array_key_exists($field, $casts)) {
+                if (class_exists($casts[$field]) && strpos($casts[$field], 'Casts') !== false) {
+                    $selectArr[] = (new $casts[$field])->select($field);
                 } else {
                     $selectArr[] = $field;
                 }
-            });
-            $builder = $builder->selectRaw(Arr::join($selectArr, ','));
-        }
+            } else {
+                $selectArr[] = $field;
+            }
+        });
+        $builder = $builder->selectRaw(Arr::join($selectArr, ','));
         return $builder;
     }
 }
